1190dc361f29cfc5a5d29f18fda5f64158517699
tatanox <95684648+tatanox@users.noreply.github.com>
2022-05-02 15:21:26 +0200

    Base_theme

---
+++ Viho-Laravel-8/theme/storage/framework/views/8874c499f88e6c4f4688dcee3264bee95dfb94b3.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/ekta/Documents/0 - Themeforest - Themes/Viho Theme/placeholder/vihotheme/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
